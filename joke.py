e8df0e63253ecadbc3702b0453b8723f98d40623
LydiaFrame <156206633+LydiaFrame@users.noreply.github.com>
2025-01-16 20:19:47 +0000

    finished

---
+++ joke.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""Module to print a joke."""
+
+__author__ = "Lydia Frame" 
+__date__ = "1/16/2025"
+
+#Print joke.
+print("Tech Support: How may I help you?") 
+print()
+print("Customer: Hi, I have got a problem.")
+print("     Your program is telling me to get a pet snake.")
+print("     I do not want one.")
+print()
+print("Tech Support: Excuse me?")
+print()
+print("Customer: It is giving me a message telling me")
+print("     I need a snake to run it")
+print()
+print("Tech Support: Read the message to me please")
+print()
+print("Customer: Error: Python required to run the script")
